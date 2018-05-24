3dc82461609ad9ae7b0cf6cd246581f0f63ba88a
Javier Cerrato <terminal069@gmail.com>
2018-05-24 12:54:17 +0200

    Initial commit

---
+++ src/main/java/es/tml/apf/ApfApplication.java
@@ -0,0 +1,12 @@
+package es.tml.apf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApfApplication.class, args);
+	}
+}
