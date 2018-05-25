9180429b3f394e5c3c090954a70f2159a951ecfe
Javier Cerrato <terminal069@gmail.com>
2018-05-25 12:08:41 +0200

    Firsts templates

---
+++ src/main/java/es/tml/apf/beans/IndexRequest.java
@@ -0,0 +1,9 @@
+package es.tml.apf.beans;
+
+import lombok.Data;
+
+@Data
+public class IndexRequest {
+
+	private String welcomeMessage;
+}
---
+++ src/main/java/es/tml/apf/controllers/FormatterController.java
@@ -0,0 +1,15 @@
+package es.tml.apf.controllers;
+
+import java.util.Map;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class FormatterController {
+
+	@RequestMapping("/formatter")
+	public void initFormatter(Map<String, Object> model) {
+		
+	}
+}
