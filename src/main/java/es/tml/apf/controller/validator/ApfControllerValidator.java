f253300eea4746714bb222b70d45a5fb21f1f45d
TML <terminal069@gmail.com>
2019-08-06 18:53:17 +0200

    Borrado

--- src/main/java/es/tml/apf/controller/validator/ApfControllerValidator.java
+++
@@ -1,8 +0,0 @@
-package es.tml.apf.controller.validator;
-
-import es.tml.apf.controller.dto.ApfRequest;
-
-public interface ApfControllerValidator {
-
-	void validateRequest(ApfRequest request);
-}
--- src/main/java/es/tml/apf/service/ApfService.java
+++
@@ -1,9 +0,0 @@
-package es.tml.apf.service;
-
-import es.tml.apf.service.dto.ApfServiceIDTO;
-import es.tml.apf.service.dto.ApfServiceODTO;
-
-public interface ApfService {
-	
-	ApfServiceODTO format(ApfServiceIDTO serviceIDTO);
-}
